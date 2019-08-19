8b15c9b54b927ed1f079ad00b64410a9889e66a3
Michael Giebner <mikegiebner@gmail.com>
2019-08-19 19:51:59 -0400

    Finished with Version 3

--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,35 @@
 // Name: Mike Giebner
 // Watch and Code Practical JavaScript
 
-var toDoList = ["item 1", "item 2", "item 3"];
+function loadToDoList() {
+  var toDoList = ["item 0", "item 1", "item 2", "item 3"];
+  return toDoList;
+}
 
-// To add an item
-toDoList.push("item 4");
+function startBlankList() {
+  return toDoList = [];
+}
 
-// To update an item
-toDoList[0] = "item 1 updated";
+// To add an item to the end of an array
+function addToList(item, toDoList) {
+  toDoList.push(item);
+  return toDoList;
+}
 
+
+// To update an item inside an array
+function updateList(num, item, toDoList) {
+  toDoList[num] = item;
+  return toDoList;
+}
+  
 // To delete an item, arg1 is where to start deleting, arg2 is the number of
 // items to delete
-toDoList.splice(0, 1);
+function deleteItems(startNum, numItems, toDoList) {
+  toDoList.splice(startNum, numItems);
+  return toDoList;
+}
+
+function displayToDoList(toDoList) {
+  console.log("My ToDo List is: ", toDoList);
+}
---
+++ toDoListV3.js
@@ -0,0 +1,44 @@
+// Name: Mike Giebner
+// Watch and Code Practical JavaScript
+
+// function loadToDoList() {
+//   var toDoList = ["item 0", "item 1", "item 2", "item 3"];
+//   return toDoList;
+// }
+
+// function startBlankList() {
+//   return toDoList = [];
+// }
+
+// // To add an item to the end of an array
+// function addToList(item, toDoList) {
+//   toDoList.push(item);
+//   return toDoList;
+// }
+
+
+// // To update an item inside an array
+// function updateList(num, item, toDoList) {
+//   toDoList[num] = item;
+//   return toDoList;
+// }
+  
+// // To delete an item, arg1 is where to start deleting, arg2 is the number of
+// // items to delete
+// function deleteItems(startNum, numItems, toDoList) {
+//   toDoList.splice(startNum, numItems);
+//   return toDoList;
+// }
+
+// function displayToDoList(toDoList) {
+//   console.log("My ToDo List is: ", toDoList);
+// }
+
+{/* <script>
+  var toDoList = {
+  toDos: ["Item 0", "Item 1", "Item 2"],
+  displayToDos: function() {
+    console.log("My ToDo List is: ", this.toDos);
+  }
+};
+</script> */}
